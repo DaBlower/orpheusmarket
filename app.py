b957b36ce457d6e55cce6468c4f12122efa69faf
DaBlower <113282214+DaBlower@users.noreply.github.com>
2025-08-26 22:32:48 +1000

    first webpage

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template_string
+from get_data import get_data
+
+app = Flask(__name__)
+
+BM_ITEMS = get_data()
+
+@app.route("/")
+def home():
+    card_html = ""
+    for item in BM_ITEMS:
+        price = item["prices"].get("XX")
+        title = item.get("title")
+        description = item.get("description")
+        image = item.get("imageUrl")
+        buy_url = item.get("purchaseUrl")
+        item_id = item.get("id")
+
+        card_html += f"""
+        <div class="card" id="card_{item_id}">
+            <img src="{image}" class="item_image" id="img_{item_id}"/>
+            <h2 class="item_title" id="title_{item_id}">{title}</h2>
+            <p class="item_description" id="desc_{item_id}">{description}</p>
+            <a href="{buy_url}"><button type="button"><img style="width: 1.5rem; padding: 0.2rem" src="https://hc-cdn.hel1.your-objectstorage.com/s/v3/6c0178740fa623a059182d076f44031600d079d5_shell.png"/>{price} needed</button></a>
+        </div>        
+        """
+    html = """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>orpheus market</title>
+    </head>
+    <body>
+        <div class="bar">
+            <h1 class="orpheusmarket">orpheusmarket</h1>
+        </div>
+        {{card_html|safe}}
+    </body>
+    </html>
+
+    """
+    return render_template_string(html, card_html=card_html)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ get_data.py
@@ -0,0 +1,21 @@
+import requests
+
+url = "https://summer.skyfall.dev/api/shop" # this is @mahad's api - if it breaks, i might make my own
+
+
+def get_data():
+    try:
+        response = requests.get(url=url).json()
+    except requests.exceptions.JSONDecodeError:
+        print(f"JSON decode error!")
+        exit()
+    except Exception as e:
+        print(f"Failed to contact api: {e}")
+
+    BM_ITEMS = []
+
+    for item in response:
+        if item["isBlackMarket"] == True:
+                BM_ITEMS.append(item)
+
+    return BM_ITEMS
